94661b75a6908f3ae51314a4591977943d5e7b4d
Erko Bridee <erko.bridee@gmail.com>
2021-02-19 08:49:50 +0100

    feat: npm business card package

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`
+Erko Bridee de Almeida Cabrera
+
+Website - https://www.erkobridee.com
+Twitter - https://www.twitter.com/erkobridee
+Github - https://www.github.com/erkobridee
+LinkedIn: https://linkedin.com/in/erkobridee
+`);
