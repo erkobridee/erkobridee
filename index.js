dd20ff79526043b7a58ed92adf3d96f9cc66504d
erkobridee <erko.bridee@gmail.com>
2024-06-19 21:06:12 +0200

    update: index.js

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ Twitter         - https://www.twitter.com/erkobridee
 Instagram       - https://instagram.com/erkobridee
 
 Github          - https://www.github.com/erkobridee
-REPL.it         - https://repl.it/@erkobridee
 Codesandbox     - https://codesandbox.io/u/erkobridee
-CodePen         - https://codepen.io/erkobridee
 Stackblitz      - https://stackblitz.com/@erkobridee
+CodePen         - https://codepen.io/erkobridee
+REPL.it         - https://repl.it/@erkobridee
+JSFiddle        - https://jsfiddle.net/user/erkobridee
 
 npmjs           - https://www.npmjs.com/org/erkobridee
 `);
